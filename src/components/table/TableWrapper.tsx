f8fd6d9b12728d062bc0257033c2a54059545831
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-11-30 17:37:22 -0300

    feat: create Button sort on TableWrapper

--- src/components/table/TableWrapper.tsx
+++ src/components/table/TableWrapper.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+
+'use client'
+import React, { useState } from 'react'
 import { FileType } from '../../../typings'
 import { Button } from '../ui/button'
+import { DataTable } from './Table'
+import { columns } from './Columns'
+import { useUser } from '@clerk/nextjs'
 
 function TableWrapper(
   { skeletonFiles }: { skeletonFiles: FileType[] }
 ) {
+  const { user } = useUser()
+  const [initialFiles, setInitialFiles] = useState<FileType[]>([])
+  const [sort, setSort] = useState<"asc" | "desc">("desc")
+
+
   return (
     <div>
       {/* button */}
-      <Button>
-        Filtrar por...
+      <Button onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')}>
+        Filtrar por {sort === 'desc' ? "Mais novo" : "Mais antigo"}
       </Button>
+
+      <DataTable columns={columns} data={skeletonFiles} />
     </div>
   )
 }
