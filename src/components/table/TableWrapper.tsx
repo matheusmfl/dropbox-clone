a8280a5b879c0847df090686fd0e9c371c3ce065
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-11-30 18:07:44 -0300

    feat: create chore for skeleton screen loadings

--- src/components/table/TableWrapper.tsx
+++ src/components/table/TableWrapper.tsx
@@ -1,11 +1,15 @@
 
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FileType } from '../../../typings'
 import { Button } from '../ui/button'
 import { DataTable } from './Table'
 import { columns } from './Columns'
 import { useUser } from '@clerk/nextjs'
+import { collection, orderBy, query } from 'firebase/firestore'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { db } from '@/firebase'
+import { Skeleton } from "@/components/ui/skeleton"
 
 function TableWrapper(
   { skeletonFiles }: { skeletonFiles: FileType[] }
@@ -14,15 +18,63 @@ function TableWrapper(
   const [initialFiles, setInitialFiles] = useState<FileType[]>([])
   const [sort, setSort] = useState<"asc" | "desc">("desc")
 
+  const [docs, loading, error] = useCollection(
+    user && (
+      query(
+        collection(db, 'users', user.id, "files"),
+        orderBy("timestamp", sort)
+      )
+    )
+  );
 
+  useEffect(() => {
+    if (!docs) return
+
+    const files: FileType[] = docs.docs.map((doc) => (
+      {
+        id: doc.id,
+        filename: doc.data().filename || doc.id,
+        timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+        fullName: doc.data().fullName,
+        downloadUrl: doc.data().downloadUrl,
+        type: doc.data().type,
+        size: doc.data().size
+      }
+    )
+
+    )
+    setInitialFiles(files)
+  }, [docs])
+
+  if (docs?.docs.length === undefined) return (
+    <div className='flex flex-col'>
+      <Button variant={"outline"} className='ml-auto w-36 h-10 mb-5'>
+        <Skeleton className='h-5 w-full' />
+      </Button>
+
+      <div className='border rounded-lg'>
+        <div className='border-b h-12' />
+        {skeletonFiles.map((file) => {
+          return (
+            <div key={file.id} className='flex items-center space-x-4 p-5 w-full'>
+              <Skeleton className='h-12 w-12' />
+              <Skeleton className='h-12 w-full' />
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+
+  console.log(initialFiles)
   return (
-    <div>
+    <div className='flex flex-col space-y-5 pb-10'>
       {/* button */}
-      <Button onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')}>
+      <Button onClick={() => setSort(sort === 'desc' ? 'asc' : 'desc')} className='ml-auto w-fit'>
         Filtrar por {sort === 'desc' ? "Mais novo" : "Mais antigo"}
       </Button>
 
-      <DataTable columns={columns} data={skeletonFiles} />
+      <DataTable columns={columns} data={initialFiles} />
     </div>
   )
 }
