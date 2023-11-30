ebbbec08399b58a27bf5bcb931b1505b05bf6b30
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-11-30 18:11:44 -0300

    feat: ending skeletons

--- src/components/table/TableWrapper.tsx
+++ src/components/table/TableWrapper.tsx
@@ -62,6 +62,13 @@ function TableWrapper(
             </div>
           )
         })}
+
+        {skeletonFiles.length === 0 && (
+          <div className='flex items-center space-x-4 p-5 w-full'>
+            <Skeleton className='h-12 w-12' />
+            <Skeleton className='h-12 w-full' />
+          </div>
+        )}
       </div>
     </div>
   )
